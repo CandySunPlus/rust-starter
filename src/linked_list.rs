356d34b365fb6eb890cebd19e15b47008842f93a
Fengming Sun <niksun@FengmingdeMacBook-Pro.local>
2021-06-16 20:46:15 +0800

    first commit

---
+++ src/linked_list.rs
@@ -0,0 +1,32 @@
+use List::*;
+
+pub enum List {
+    Cons(u32, Box<List>),
+    Nil,
+}
+
+impl List {
+    pub fn new() -> Self {
+        Nil
+    }
+
+    pub fn prepend(self, elem: u32) -> Self {
+        Cons(elem, Box::new(self))
+    }
+
+    pub fn len(&self) -> u32 {
+        match *self {
+            Cons(_, ref tail) => 1 + tail.len(),
+            Nil => 0,
+        }
+    }
+
+    pub fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                format!("{}, {}", head, tail.stringify())
+            }
+            Nil => format!("Nil"),
+        }
+    }
+}
---
+++ src/rectangle.rs
@@ -0,0 +1,36 @@
+#[derive(Debug)]
+pub struct Point {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Debug)]
+pub struct Rectangle {
+    p1: Point,
+    p2: Point,
+}
+
+impl Point {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self { x, y }
+    }
+}
+
+impl Rectangle {
+    pub fn rect_area(&self) -> f32 {
+        let Rectangle {
+            p1: Point { x: x1, y: y1 },
+            p2: Point { x: x2, y: y2 },
+        } = self;
+        return (y2 - y1) * (x2 - x1);
+    }
+}
+
+pub fn square(p1: Point, wh: f32) -> Rectangle {
+    let Point { x: x1, y: y1 } = p1;
+    let p2 = Point {
+        x: x1 + wh,
+        y: y1 + wh,
+    };
+    return Rectangle { p1, p2 };
+}
